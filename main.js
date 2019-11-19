08d7d24342e9dbd77bc4b7fc45d5ae5098055630
phenix1229 <paul.garay@codeimmersives.com>
2019-11-19 16:03:23 -0500

    created roll functions and 2 average functions

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ const sixes = [];
 const doubleSixes = [];
 const twelves = [];
 const twenties = [];
-
+// const sixResults = [];
+// const doubleSixResults = [];
+// const twelveResults = [];
+// const twentyResults = [];
 
 /*************************
  * RANDOM ROLL GENERATOR *
@@ -26,6 +29,53 @@ function getRandomNumber(max) {
  * YOUR CODE BELOW *
  *******************/
 
+function rollSix(){
+    const result = getRandomNumber(6);
+    sixes.push(result);
+    return result;
+}
+
+function rollDoubleSix(){
+    const result = getRandomNumber(12);
+    sixes.push(result);
+    return result;
+}
+
+function rollTwelve(){
+    const result = getRandomNumber(12);
+    sixes.push(result);
+    return result;
+}
+
+function rollTwenty(){
+    const result = getRandomNumber(20);
+    sixes.push(result);
+    return result;
+}
+
+function getMean(arr){
+    const mean = arr.reduce(function(a,b){return a + b}, 0) / arr.length;
+    return mean;
+}
+
+function getMedian(arr){
+    const srtArr = arr.sort(function(a,b){return a - b});
+    const mid1 = arr[srtArr.length / 2];
+    const mid2 = arr[(srtArr.length / 2) -1];
+    const middle = [mid1, mid2];
+
+    if (arr.length % 2 === 0){
+        median = getMean(middle);
+    } else if (arr.length % 2 === 1){
+        median = arr[(arr.length - 1) / 2]
+    }
+
+    return median;
+}
+
+function getMode(arr){
+    
+}
 
 /*******************
  * EVENT LISTENERS *
